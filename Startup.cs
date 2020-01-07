cc4571ef3c41cd0529d46888399daf5060913965
Michaela Icmat <icmat@shernet.sheridancollege.ca>
2020-01-06 20:54:26 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MSIPortfolio.Startup))]
+namespace MSIPortfolio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
